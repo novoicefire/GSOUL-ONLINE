5f43460a4f4ffd9b129653a5a2c11fdb9eb87f91
novoicefire <ofireo1023@gmail.com>
2025-12-29 02:13:09 +0800

    .

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'gsoul-online-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './admin_dashboard.html',
+  './annual_review.html',
+  './GSoulLogo.png',
+  './player_character.png',
+  './player_character_2.png',
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
